24d858f4aa2773437091c8c6d5511aeb7b54de5b
Kevin Howard <khoward5@nd.edu>
2019-07-14 15:14:24 -0400

    added specific heat and susceptibility functions, not yet tested

--- examples/ising_2D.py
+++ examples/ising_2D.py
@@ -28,11 +28,23 @@ def boltz_factor(e_final=0.0,e_initial = 1.0,k=1,T=1):
     exponent = -(e_final-e_initial) / k / T
     return np.exp(exponent)
 
+def heat_capacity_calc(energy,energy2,T,boltz_constant=1):
+    num = energy2 - energy*energy
+    denom = boltz_constant * T *T
+    return num/denom
+
+def susceptibility_calc(M,M2,T,boltz_constant=1):
+    num = M2 - M*M
+    denom = boltz_constant * T
+    return num/denom
+
 def metropolis(temperature=1,J=1,boltz_constant=1,N_grid=10,N_steps=2000,burn_in=1000,save_chain = True):
     spins = random.choice([-1.0,1.0],size=(N_grid,N_grid))
     energy = ising_energy(spins,J=J)
     en = np.zeros(N_steps)
+    en2 = np.zeros(N_steps)
     mags = np.zeros(N_steps)
+    mags2 = np.zeros(N_steps)
     for j in range(N_steps+burn_in):
         spinIndex = random.randint(0,N_grid,size=(2))
         spinIndex = spinIndex[0],spinIndex[1]
@@ -53,39 +65,55 @@ def metropolis(temperature=1,J=1,boltz_constant=1,N_grid=10,N_steps=2000,burn_in
             continue
         else:
             en[j-burn_in]=(energy/spins.size)
+            en2[j-burn_in] = (energy/spins.size)*(energy/spins.size)
             mag = ising_magnetization(spins)
             mags[j-burn_in] = mag
+            mags2[j-burn_in] = mag*mag
     if save_chain == True:
-        return en,mags
+        return en,mags,en2,mags2
     else:
-        return en.mean(),mags.mean()
+        return en.mean(),mags.mean(),en2.mean(),mags2.mean()
 
 
 N = 10
-fig, ax = plt.subplots(2,1,figsize=(12/2,9/2),sharex=True)
-temps = np.arange(0.1,5.0,0.1)
+fig, ax = plt.subplots(2,2,figsize=(12/2,9/2),sharex=True)
+temps = np.arange(1,3,0.025)
 energies = np.zeros(temps.size)
 magnetizations = np.zeros(temps.size)
+heat_capacities = np.zeros(temps.size)
+susceptibilities = np.zeros(temps.size)
+
 for index,t in enumerate(temps):
-    en,mags = metropolis(N_steps=1500000,temperature=t,N_grid=N,save_chain=False)
+    en,mags,en2,mags2 = metropolis(N_steps=7500000,temperature=t,N_grid=N,save_chain=False,burn_in=10000)
     energies[index] = en
     magnetizations[index] = np.abs(mags)
-ax[0].plot(temps,energies,c='k')
-ax[1].plot(temps,magnetizations,c='r')
-# en *= 2**(index+1)
-# mags *= 2**(index*1)
+    heat_capacities[index] = heat_capacity_calc(en,en2,t)
+    susceptibilities[index] = susceptibility_calc(mags,mags2,t)
 
 
-ax[0].set_ylim(-2.1,0.1)
-ax[0].set_yticks([-2,-1,0])
-ax[0].set_ylabel("E [arb]")
+ax[0][0].plot(temps,energies,c='k')
+ax[0][1].plot(temps,magnetizations,c='k')
+ax[1][0].plot(temps,heat_capacities,c='k')
+ax[1][1].plot(temps,susceptibilities,c='k')
 
-ax[1].set_ylim(-0.1,1.1)
-ax[1].set_ylabel("M [arb]")
-ax[1].set_xlabel("Temperature [arb]")
+ax[0][0].set_ylim(-2.1,0.1)
+ax[0][0].set_yticks([-2,-1,0])
+ax[0][0].set_ylabel("E [arb]")
+
+ax[0][1].set_ylim(-0.1,1.1)
+ax[0][1].set_ylabel("M [arb]")
+ax[0][1].set_xlabel("Temperature [arb]")
+
+
+#ticks,lim
+ax[1][0].set_ylabel("$C_{V}$ [arb]")
+
+#ticks, lim
+ax[1][1].set_ylabel("$\chi$ [arb]")
+
 
 plt.show()
-fig.savefig("plot.pdf",bbox_inches='tight')
+fig.savefig("biggest_burn_in_plot.pdf",bbox_inches='tight')
 
 
 
