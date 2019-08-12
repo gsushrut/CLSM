fbc2ca76ed27c6da83f9cbf1b4ab3c2ea1992a9d
gsushrut <23340812+gsushrut@users.noreply.github.com>
2019-08-12 15:24:17 -0400

    Generate random network

---
+++ examples/Project 4/randomgen.m
@@ -0,0 +1,9 @@
+conn=eye(N);  %This is the connection matrix
+parfor i=1:N
+    for j=1:N
+        temp=rand;  %Generates a random number between 0 and 1
+        if (temp < f)
+            conn(i,j)=1;    %Creates an edge with the given probability
+        end
+    end
+end
